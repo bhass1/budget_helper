cfeacb5ff2a8df047ca5022721f2d2f6753a1206
Bill Hass <billhass@umich.edu>
2022-08-14 17:20:20 -0400

    feat: pytest passing 1 test

--- README.md
+++ README.md
@@ -16,7 +16,16 @@ Tired of tediously categorizing my past expenses every couple months. Often it's
 
 ```
 docker build . -t budget_helper
-docker run --rm -it -v $PWD/output:/usr/src/app/output budget_helper python ./main.py test/test-simple/test-categories.yml test/test-simple/test-amex-credit-simple.csv test/test-simple/test-chase-credit-simple.csv output/test-simple/out-simple.xlsx
+docker run --rm -it -v $PWD/output:/usr/src/app/output budget_helper python ./src/budget_helper_bhass1/main.py tests/test-simple/test-categories.yml tests/test-simple/test-amex-credit-simple.csv tests/test-simple/test-chase-credit-simple.csv output/test-simple/out-simple.xlsx
+```
+
+### Testing
+
+**_Note: You need to be in the root directory of this project (e.g. via `cd budget_helper`)_**
+
+```
+docker build . -t budget_helper
+docker run --rm -it budget_helper pytest
 ```
 
 ### Architecture
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,19 +1,28 @@
-import tempfile
+import os
 import pandas as pd
-from .context import budget_helper_bhass1 as bhb
+import sys
+import tempfile
+import logging
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "src", "budget_helper_bhass1"))
+import expensecategorizer as ec
 
 def test_simple(tmp_path):
 
-    test_out = tmp_path / 'test-simple-out.xlsx'
-    expenseCat = bhb.ExpenseCategorizer(
-        'test-simple/test-categories.yml',
+  test_out = tmp_path / 'test-simple-out.xlsx'
+  expenseCat = ec.ExpenseCategorizer(
+      'tests/test-simple/test-categories.yml',
+      ['tests/test-simple/test-amex-credit-simple.csv', 'tests/test-simple/test-chase-credit-simple.csv'],
+      test_out
+  )
 
-        ['test-simple/test-amex-credit-simple.csv', 'test-simple/test-chase-credit-simple.csv'],
-        test_out
-    )
+  expenseCat.one_shot()
 
-    expenseCat.one_shot()
-
-    df_test_out = pd.read_excel(test_out)
-    df_test_golden = pd.read_excel('test-simple/test-out-simple.xlsx')
-    assert df_test_out.equals(df_test_golden)
+  logging.getLogger().setLevel(logging.INFO)
+  df_test_out = pd.read_excel(test_out)
+  df_test_out['TransactionDate'] = pd.to_datetime(df_test_out.TransactionDate)
+  logging.info(df_test_out)
+  df_test_golden = pd.read_excel('tests/test-simple/test-out-simple.xlsx')
+  df_test_golden['TransactionDate'] = pd.to_datetime(df_test_golden.TransactionDate)
+  logging.info(df_test_golden)
+  assert df_test_out.equals(df_test_golden)
